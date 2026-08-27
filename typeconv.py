e7914ae4621991cb1c94fa43feefaa8f37e944ba
user.name like : Insiya <zaidiinsiya83@gmail.com>
2026-08-27 17:37:59 +0530

    conv

--- typeconv.py
+++ typeconv.py
@@ -14,3 +14,9 @@ print(a)
 """b = "abbdj"  we cant convert a string to int 
 b = int(b)
 print(type(b))"""
+
+# implicit conv - in this python automatically converts data from one data type to another 
+# explicit conv - in this user use inbuild func to  converts data from one data type to another 
+d = 12/4
+print(d)  # 3.0 not just 3 
+print(type(d))
