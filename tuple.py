6de6dc1758bf7b910d6479618057da7788f3ebbd
user.name like : Insiya <zaidiinsiya83@gmail.com>
2026-08-29 23:33:37 +0530

    29

---
+++ tuple.py
@@ -0,0 +1,30 @@
+a=(1,2,3,4 , "hello" , 3 )  # tuple is immutable - cannot change values in tuple 
+# we can  have duplicates , ordered can access using idx , heterogeneous 
+#print (type(a))
+
+# for i in a:
+#     print(i)
+
+# for i in range(len(a)):
+#         print(a[i])
+
+# idx = a.index("hello")
+# print(idx)
+
+# countof3 = a.count(3)
+# print(countof3)
+
+# TUPLE UNPACKING - 
+# g , h , i ,j = (2 , 4 , 6 , 8 )
+# print(g)
+# print(h)
+# print(i)
+# print(j)
+
+# print(type(g))  # it will be int 
+
+# IF WE DONT WANT TUPLE UNPACKING 
+
+k = (2,)
+print(type(k)) # now it will be tuple 
+print(k)
