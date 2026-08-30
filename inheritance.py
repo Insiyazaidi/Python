7594a3f1a2e3c252a9a27b246fb87281c4f6b7dc
user.name like : Insiya <zaidiinsiya83@gmail.com>
2026-08-31 01:04:22 +0530

    inheritence

--- inheritance.py
+++ inheritance.py
@@ -1,11 +1,23 @@
 class Factory():
     a = "attribute inside factory , parent class "
+    def __init__(self , name , age):  # this is consturctor 
+        print(f"{name} , {age}")
+
+
     def hello(self): 
         print("hello from factory , parent class ")
 
 class Pune(Factory):  # child class 
-    pass 
+    def __init__(self , name , age , location):
+        super().__init__(name , age)  # from parent class 
+        self.location = location  # child class only 
 
-obj = Factory()
+        print(f"{name} , {age} , {location} from child class")
+
+
+obj = Factory("insiya" ,23)
 print(obj.a)
 obj.hello()
+
+obj2 = Pune("zoya" , 1 , "krishna nagar")  # it can access hello from parent 
+obj2.hello()
