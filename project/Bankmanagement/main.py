804fd830d11295da1b51cff0e0cc695e6cb0c97a
user.name like : Insiya <zaidiinsiya83@gmail.com>
2026-09-02 01:36:35 +0530

    2sep

--- project/Bankmanagement/main.py
+++ project/Bankmanagement/main.py
@@ -9,16 +9,28 @@ class Bank:
    data =[]
 
    try:
-        if Path(database).exists():
-         with open(database , 'r') as fs:
-          data = json.loads(fs.read())
-
+        if Path(database).exists():  # Path Python ka object banata hai jo file/folder ke path ko represent karta hai.
+         with open(database , 'r') as fs: # open() ko file ka naam ya file ka path, dono de sakte ho.
+          data = json.loads(fs.read())  
+        else:
+          print("no such file exist ")
    except Exception as err:
      print(f"an exception occ as {err}")
 
 
+# json.loads() JSON ko Python object mein convert karta hai, aur json.dumps() Python object ko JSON format mein convert karta hai.
+
 def createaccount(self):
-  pass
+   data = {
+     "name" : input("Tell your name"),
+      "age" :int(input("tell your age")),
+      "email" : (input("tell your email")),
+       "pin" : int(input("tell your pin")),
+       "accountNo": 1234,
+       "balance":0
+}
+   if data["age"] <18 or len(str(data["pin"]))!=4:  # pin jo int m tha ab 
+      print("sorry u cannot create account")
 
 
 
