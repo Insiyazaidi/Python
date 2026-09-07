76b6638dd722895928cbd90b4a3b872b2f434d15
user.name like : Insiya <zaidiinsiya83@gmail.com>
2026-09-08 01:05:19 +0530

    8 sep

--- project/Bankmanagement/main.py
+++ project/Bankmanagement/main.py
@@ -126,6 +126,7 @@ class Bank:
      print("your information are \n\n\n")
      for i in userdata[0]: # 0 th  element mean first element 
        print(f"{i} : {userdata[0][i]}")
+
      
    def updatedetails(self):
       accnumb =   input("please tell your account numb")
@@ -139,8 +140,9 @@ class Bank:
         newdata = {
           "name": input("please tell new name or press enter :"),
           "email": input("please tell new email or press enter :"),
-           "pin": input("please tell new name or press enter :")
+           "pin": input("please tell new pin or press enter :")
         }
+       
         if newdata["name"] =="": # if empty ie user do not want to change it -- then fill whatever was there previously 
           newdata["name"] = userdata[0]["name"]
         if newdata["email"] =="":
@@ -159,10 +161,23 @@ class Bank:
       for i in newdata:
        if newdata[i]==userdata[0][i]: # agr same h dono m toh continue 
           continue
-      else:
-        userdata[0][i] = newdata[i]
-  
+       else:
+         userdata[0][i] = newdata[i]
 
+      print(userdata)
+      Bank.__update()
+      print("Details updated succ ")
+
+
+   def delete(self):
+      accnumb =   input("please tell your account numb")
+      pin =   int(input("please tell your pin "))
+      userdata = [i for i in Bank.data if i["accountNo"] == accnumb and i["pin"] == pin]
+      if userdata == False:
+             print("No such data found")
+      else:
+        check = input("press y if you want to delete the account ")
+    
 user = Bank()  
 
 
@@ -189,3 +204,5 @@ if check ==4:
 if check ==5:
   user.updatedetails()
 
+if check ==6:
+  user.delete()
